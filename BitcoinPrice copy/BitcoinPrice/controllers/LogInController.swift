a002fa29b502ed5e146ffbaacbdd1890ab399c98
basitali99 <90155707+basitali99@users.noreply.github.com>
2021-09-15 20:47:07 -0400

    Add files via upload

---
+++ BitcoinPrice copy/BitcoinPrice/controllers/LogInController.swift
@@ -0,0 +1,22 @@
+//
+//  LogInController.swift
+//  BitcoinPrice
+//
+//  Created by basit ch on 9/15/21.
+//
+
+import UIKit
+
+class LogInController: UIViewController {
+    
+    @IBOutlet weak var userNameText: UITextField!
+    
+    @IBOutlet weak var passwordText: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
---
+++ BitcoinPrice copy/BitcoinPrice/models/NetworkManager.swift
@@ -0,0 +1,25 @@
+//
+//  NetworkManager.swift
+//  BitcoinPrice
+//
+//  Created by basit ch on 9/15/21.
+//
+
+import Foundation
+
+struct NetworkManager{
+    
+    static var shared = NetworkManager()
+    
+    func serviceCall(url: URLRequest, completion: @escaping (Any?,HTTPURLResponse,ErrorTypes?) ->()){
+        URLSession.shared.dataTask(with: url) {data , response , error in
+            let Response = response as! HTTPURLResponse
+            if error == nil{
+                completion(data,Response,nil)
+            }else{
+                completion(nil,Response,.NetworkingError)
+            }
+        }.resume()
+    }
+    
+}
